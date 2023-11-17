13f8134d36c1a3d3cb1c3c28fc5afc2a3f3bcda1
Aayush Tiwari <tiwariaayush682@gmail.com>
2023-11-17 21:02:33 +0530

    add cors

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT;
+const cors = require('cors');
 const fileRoute = require('./routes/fileRoute');
 const showFileRoute = require('./routes/showFile');
 const downloadFileRoute = require('./routes/downloadFile');
 
+//Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
 //Middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 
 //Templates Engines
 app.set('views', path.join(__dirname, '/views'));
